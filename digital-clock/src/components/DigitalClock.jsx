fc9080dd4fe7cfd273dae174e4027b7eba36e36e
DIR\guray.cintir <guray.cintir@accenture.com>
2025-04-09 14:16:47 +0300

    dark mode flow

--- digital-clock/src/components/DigitalClock.jsx
+++ digital-clock/src/components/DigitalClock.jsx
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from "react";
 import * as dateTimeUtil from "../util/DateTimeUtil.js";
+import { FaMoon } from "react-icons/fa";
+import { WiDaySunny } from "react-icons/wi";
 
 function DigitalClock() {
   const [time, setTime] = useState(new Date());
   const [hourFormat24, setHourFormat24] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
 
   useEffect(() => {
     const timerId = setInterval(() => {
@@ -25,16 +32,52 @@ function DigitalClock() {
   const { day, month, year } = dateTimeUtil.getFormattedDate(time);
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-200">
-      <div className="flex flex-col gap-y-5 p-5 justify-center items-center bg-gray-200 rounded-3xl shadow-2xl w-auto h-auto">
-        <h1 className="font-extrabold text-4xl">Digital Clock</h1>
-        <p className="font-extralight text-2xl mb-5">
+    <div
+      className={`flex flex-col gap-y-32 justify-center items-center h-screen ${
+        darkMode ? "bg-gray-600" : "bg-gray-200"
+      } transition duration-300`}
+    >
+      <div>
+        <button onClick={toggleDarkMode}>
+          {darkMode ? (
+            <WiDaySunny color="white" size={32} className="text-2xl" />
+          ) : (
+            <FaMoon size={32} className="text-2xl" />
+          )}
+        </button>
+      </div>
+
+      <div
+        className={`flex flex-col gap-y-5 p-5 justify-center items-center rounded-3xl shadow-2xl w-auto h-auto ${
+          darkMode ? "bg-gray-600" : "bg-gray-200"
+        } transition duration-300`}
+      >
+        <h1
+          className={`font-extrabold text-4xl ${
+            darkMode ? "text-blue-200" : "text-black"
+          } transition duration-300`}
+        >
+          Digital Clock
+        </h1>
+        <p
+          className={`font-extralight text-2xl mb-5 ${
+            darkMode ? "text-blue-200" : "text-black"
+          } transition duration-300`}
+        >
           Display current time in hours, minutes and seconds
         </p>
-        <h1 className="font-extrabold text-8xl">
+        <h1
+          className={`font-extrabold text-8xl ${
+            darkMode ? "text-blue-200" : "text-black"
+          } transition duration-300`}
+        >
           {hours}:{minutes}:{seconds} {ampm}
         </h1>
-        <h1 className="font-extralight text-2xl mb-5">
+        <h1
+          className={`font-extralight text-2xl mb-5 ${
+            darkMode ? "text-blue-200" : "text-black"
+          } transition duration-300`}
+        >
           {day} {month},{year}
         </h1>
         <div className="flex gap-x-5">
