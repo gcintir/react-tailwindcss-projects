5447b4e0e925f7513da2f87df12ba16ddb9278a7
DIR\guray.cintir <guray.cintir@accenture.com>
2025-04-15 23:50:56 +0300

    simple responsive navbar

--- responsive-navbar/src/App.jsx
+++ responsive-navbar/src/App.jsx
@@ -1,9 +1,9 @@
+import NavBar from "./components/NavBar";
+
 function App() {
   
   return (
-    <h1 className="bg-gray-300 text-center text-3xl font-bold underline">
-      Hello World
-    </h1>
+    <NavBar />
   );
 }
 
